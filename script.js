0def85f4a583c28cd891fee57a3aa68cea68daeb
Ask200 <72296676+Ask200@users.noreply.github.com>
2021-06-20 08:41:32 +0530

    responsive,section2
     Responsive 1st section done,2nd section done  please see the blocks class and in it box class and do the changes in it positioning .

---
+++ script.js
@@ -0,0 +1,43 @@
+//sroll down fade effect.
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $(".title").css({
+        "opacity": "0"
+      })
+
+    } else {
+      $(".title").css({
+        "opacity": "1"
+      })
+
+
+    }
+  })
+
+
+//for smoothe scroll effect. Not working
+$(document).ready(function(){
+
+  $(".icon").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
